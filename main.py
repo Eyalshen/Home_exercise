a23ac573cfc63507f1d4f380af513d9310f1b671
Eyalshen <Eialsh@mta.ac.il>
2021-09-04 10:06:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,21 +3,29 @@ import datetime
 from suntime import Sun
 from matplotlib import pyplot as plt
 
+'''
+program explanation :
+* The program getting csv of RSDs ,and using it to create data frame
+* the program uses the function filter_out_night_RSDs to filtering out night time RSDs
+* the program also uses the function plot_DF to prepare the plot 
+* the program uses some assumptions :
+    1.  Each data frame will have those fileds : "rsd_time" , "location" :
+        - This assumption uses to manipulate and filter the data frame 
+    2.  All the locations of the RSDs are close to each other
+        - This assumption leads us to create single sun Object
+    3.  The data contains about 1 million RSDs from a range of about 180 days
+        - This assumption leads us to use dynamic dictionary of suns times by the following rules : 
+             * searching for suns time by given date in the dictionary
+             * if  suns time was found then return it
+             * else create the suns time of given date and insert it to the dictionary 
+'''
 
-# TODO: clean the code
-# TODO: documantation
-# TODO: matplotlib
 
-# this function get suntime dictionary , sun object , timestamp , location
-# and returns suntime of the location and date
-def get_suntime(suntime_dict, sun, timestamp, location):
+
+def get_suntime_by_location_and_date(dict, sun, timestamp):
+    # this function gets sun times dictionary ,sun object , timestamp and location returns the sunrise and sunset timestamp
+    # this method uses dictionary to avoid searching of suntimes that were already created
     date = get_RSD_date(int(timestamp))
-    return get_suntime_by_given_location_and_date(suntime_dict, sun, location, date)
-
-
-# this function gets location and date and returns the sunrise and sunset timestamp
-# this method use dictionary to avoid of creating suntimes that was already created
-def get_suntime_by_given_location_and_date(dict, sun, locations, date):
     if date not in dict.keys():
         sunrise = sun.get_local_sunrise_time(date)
         sunset = sun.get_local_sunset_time(date)
@@ -27,16 +35,16 @@ def get_suntime_by_given_location_and_date(dict, sun, locations, date):
     return dict[date]
 
 
-# This function get timestamp and returns the date
 def get_RSD_date(timestamp):
+    # This function get timestamp and returns the date
     my_date = datetime.datetime.fromtimestamp(timestamp / 1000.0)
     return my_date.date()
 
 
-# This method return if its night time
-def is_daytime(suntime_dict, sun, timestamp, location):
-    sunrise, sunset = get_suntime(suntime_dict, sun, timestamp, location)
-    return (sunrise <= timestamp and timestamp <= sunset)
+def is_daytime(suntime_dict, sun, timestamp):
+    # This method return if its day time
+    sunrise, sunset = get_suntime_by_location_and_date(suntime_dict, sun, timestamp)
+    return sunrise <= timestamp and timestamp <= sunset
 
 
 def initialize_sun_by_df_row(df_row):
@@ -45,42 +53,40 @@ def initialize_sun_by_df_row(df_row):
     return Sun(float(locations_list[0]), float(locations_list[1]))
 
 
-
-# this function get dataframe of RSDs and filtering out the
-def filtering_out_night_RSDs(input_dataframe):
+def filter_out_night_RSDs(input_dataframe):
+    # this function gets a dataframe of RSDs and filters out the night RSDs
     sun = initialize_sun_by_df_row(input_dataframe.loc[0])
     suntime_dict = dict()
 
-    input_dataframe["is day"] = [is_daytime(suntime_dict, sun, timestamp, location) for (timestamp, location) in
-                             zip(input_dataframe['rsd_time'], input_dataframe['location'])]
-    nighttime_df = input_dataframe.loc[input_dataframe["is day"] == False]
+    input_dataframe["is day"] = [is_daytime(suntime_dict, sun, timestamp) for (timestamp) in
+                                 input_dataframe['rsd_time']]
+    daytime_dataframe = input_dataframe.loc[input_dataframe["is day"] == True]
 
     # cleaning and restoring the DataFrame
-    del input_dataframe['is day']
-    del nighttime_df['is day']
-    return nighttime_df
+    input_dataframe = input_dataframe.drop(columns=['is day'])
+    daytime_dataframe = daytime_dataframe.drop(columns=['is day'])
 
+    return daytime_dataframe
 
-
-def plotting_df(dataframe, my_plot, i_color):
-    list_dates = [get_RSD_date(timestamp) for timestamp in dataframe['rsd_time']]
-    list_minutes = [rsd_time_to_plot(timestamp) for timestamp in dataframe['rsd_time']]
-    my_plot.scatter(list_dates, list_minutes, s=0.5, color=i_color, alpha=0.6)
-
-
+def plot_DF(dataframe, my_plot, i_color):
+    # This function gets dataframe , plot , color and preparing the plot to show
+    plot_list = [rsd_time_to_plot(timestamp) for timestamp in dataframe['rsd_time']]
+    my_plot.scatter(*zip(*plot_list), s=0.5, color=i_color, alpha=0.6)
 
 def rsd_time_to_plot(timestamp):
+    # This function get timestamp and returns tuple of date and minutes
     my_date = (datetime.datetime.fromtimestamp(timestamp / 1000.0))
     date = my_date.date()
     minutes = my_date.time().hour * 60 + my_date.time().minute
-    return minutes
+    return (date,minutes)
 
 
 if __name__ == '__main__':
     input_plot = plt
     plt.style.use('Solarize_Light2')
     df = pd.read_csv("Query_IL.csv")
-    new_df = filtering_out_night_RSDs(df)
-    # plotting_df(df, input_plot, "darkorange")
-    # plotting_df(new_df, input_plot, "cornflowerblue")
-    # input_plot.show()
+    new_df = filter_out_night_RSDs(df)
+    # print(new_df)
+    plot_DF(df, input_plot, "cornflowerblue")
+    plot_DF(new_df, input_plot, "darkorange")
+    input_plot.show()
